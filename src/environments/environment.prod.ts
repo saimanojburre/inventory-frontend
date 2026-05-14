83ad8bfb4602e388c82abd809ca47cd3cd98a256
saimanojburre <saimanoj.burre@gmail.com>
2026-05-14 17:09:47 +0530

    changed api env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   // apiBaseUrl: 'https://inventory-backend-production-a22a.up.railway.app',
-  apiBaseUrl: 'https://inventory-api.azurewebsites.net',
+  apiBaseUrl: 'https://inventory-backend-production-6145.up.railway.app',
 };
